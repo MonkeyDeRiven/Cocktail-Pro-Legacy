c7265d7dd7657a0ceb5331271d8356a4a8de3e53
Arian Doubravsky <arian.doubravsky@stud.h-da.de>
2021-11-05 20:42:13 +0000

    Update Entleerer.h

--- src/main/Entleerer.h
+++ src/main/Entleerer.h
@@ -9,19 +9,47 @@
 #include "InternalDevice.h"
 #include "Waage.h"
 
+/**
+*Subsystem
+*@class Entleerer
+*
+*@brief this class executes the output of ingredients.
+*/
 class Entleerer : public InternalDevice, public Observer {
 public:
+    /**
+    *@brief constructor creates a pointer to the created object
+    *@return a pointer to the created object
+    *@param g is the amount in gram
+    *@param ze is the number of time units
+    *@param wg is  a pointer to the specific Waage
+    */
     Entleerer(float g, int ze, Waage * wg);
+    /**
+    *@brief destructor for Entleerer
+    */
     ~Entleerer();
+    /**
+    *@brief this method regulates the amount of output in the cocktail glass
+    */
     void update();
-
+    /**
+    *@brief this method executes the output of cocktail that gets poured in the glass
+    */
     void doIt(float value);
 
 private:
+    /**
+    *object of float
+    */
     float grammProZeit;
-
+    /**
+    *object of Waage
+    */
     Waage * myWaage;
-
+    /**
+    *object of float
+    */
     float value;
 
 };
