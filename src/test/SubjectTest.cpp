718d50dc5f594f36d64cd9b0730b2ecdcb13ab89
Kaan Evlende <kaan.evlende@stud.h-da.de>
2021-11-28 23:17:59 +0000

    Revert "Merge branch 'test_branch_KE' into 'master'"
    This reverts merge request !18

--- src/test/SubjectTest.cpp
+++ src/test/SubjectTest.cpp
@@ -1,41 +1,4 @@
-#include <limits.h>
-#include "gtest/gtest.h"
+//
+// Created by arian on 23/11/2021.
+//
 
-#define protected public
-#define private public
-#include "Subject.h"
-#include "Dosierer.h"
-#include "Waage.h"
-#undef protected
-#undef private
-
-class Subjecttest : public ::testing::Test
-{protected:
-  Waage *r3;
-  Dosierer *r2;
-  Subject *r;
-  virtual void SetUp() {
-    r3 = new Waage();
-    r2 = new Dosierer(1,1,"Test3",r3);
-    r = new Subject();
-
-  }
-  virtual void TearDown()
-  {
-    delete r2;
-    delete r;
-    delete r3;
-  }
-
-
-};
-
-TEST_F(Subjecttest,attach)
-{
-r->attach(r2);
-EXPECT_EQ(r->observers.size(),1);
-}
-TEST_F(Subjecttest,notify)
-{
-EXPECT_EQ(r->observers.size(),0);
-}
