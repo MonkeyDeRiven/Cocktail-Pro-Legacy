afc351ac75e8456d18e91d89693559a837737554
Ninh, Sophia <sophia.ninh@stud.h-da.de>
2021-11-28 07:41:39 +0100

    Tests

--- src/main/Schuettler.cpp
+++ src/main/Schuettler.cpp
@@ -1,18 +1,12 @@
 //@(#) Schuettler.cpp
 
-#include <sys/time.h>
 #include "Schuettler.h"
 
 Schuettler::Schuettler() : InternalDevice() {
 }
 
-void Schuettler::doIt(float workTime) {
-  //time_t start = time(nullptr);
-  //time_t msStart = start * 1000;
-  myTimer->sleep_in_intervals(workTime * 1000); // uses the function of the class time
+void Schuettler::doIt(float time) {
+  myTimer->sleep_in_intervals(time * 1000); // uses the function of the class time
   std::cout << std::endl;
-  /*time_t end = time(nullptr);
-  time_t msEnd = end * 1000;
-  float neededTime = msEnd-msStart;
-  return neededTime;*/
+
 }
