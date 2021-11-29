6462229a0e52c847d68a3e516a7360f0096b7044
Maximilian <stmnhirs@stud.h-da.de>
2021-11-29 01:40:18 +0100

    Hotfix 2.0

--- src/test/MixerTest.cpp
+++ src/test/MixerTest.cpp
@@ -11,15 +11,43 @@
 #undef protected
 #undef private
 
-class MixerTest : public ::testing::Test{
+#include <limits.h>
+#include "gtest/gtest.h"
+
+#define protected public
+#define private public
+#include "Mixer.h"
+#undef protected
+#undef private
+
+class MixerTest : public ::testing::Test {
  protected:
+  Mixer *r;
+  float turbo = 20;
+  std::basic_streambuf<char> *old_buf;
+  std::stringstream ss;
 
-  virtual void setup(){
-
+  virtual void SetUp() {
+    old_buf = std::cout.rdbuf(ss.rdbuf());
+    r = new Mixer();
+    r->myTimer->set_Turbo(turbo);
   }
 
-  virtual void tearDown(){
-
+  virtual void TearDown() {
+    std::cout.rdbuf(old_buf); // redirect cout back to standard cout
+    delete r;
   }
 
 };
+
+TEST_F(MixerTest,doIt) {
+  float t = 20;
+  time_t start = time(nullptr);
+  time_t msStart = start * 1000;
+  r->doIt(t);
+  time_t end = time(nullptr);
+  time_t msEnd = end * 1000;
+  float neededTime = msEnd - msStart;
+  float expTime = 20000;
+  EXPECT_FLOAT_EQ(neededTime*turbo, expTime);
+}
--- src/test/WaageTest.cpp
+++ src/test/WaageTest.cpp
@@ -2,3 +2,47 @@
 // Created by arian on 23/11/2021.
 //
 
+#include <limits.h>
+#include "gtest/gtest.h"
+
+#define protected public
+#define private public
+#include "Waage.h"
+
+#undef protected
+#undef private
+
+class Waage_Test : public ::testing::Test
+{protected:
+  Waage *r;
+
+  virtual void SetUp() {
+    r = new Waage();
+    r->changeWeight(10);
+  }
+  virtual void TearDown()
+  {
+    delete r;
+  }
+};
+
+TEST_F(Waage_Test,getWeight)
+{
+  EXPECT_EQ(r->getWeight(),10);
+}
+
+TEST_F(Waage_Test,changeWeight)
+{r->changeWeight(20);
+  EXPECT_EQ(r->getWeight(),30);
+}
+
+TEST_F(Waage_Test,getDelta)
+{
+
+  EXPECT_EQ(r->getDelta(),10);
+}
+
+TEST_F(Waage_Test, tara){
+  r->tara();
+  EXPECT_EQ(r->getDelta(),0);
+}
