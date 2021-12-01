58696cd228f88f92e46aca67941c032d90ee19e6
Maximilian <stmnhirs@stud.h-da.de>
2021-12-01 16:29:36 +0100

    Zubereiter Tests

---
+++ src/test/CocktailZubereiterTest.cpp
@@ -0,0 +1,43 @@
+//
+// Created by Max on 01.12.2021.
+//
+
+
+#include <limits.h>
+#include "gtest/gtest.h"
+
+#define protected public
+#define private public
+#include "CocktailZubereiter.h"
+#undef protected
+#undef private
+
+class CocktailZubereiterTest : public ::testing::Test {
+ protected:
+  VorhandeneZutaten* vz;
+  DeviceVerwalter* dv;
+  CocktailZubereiter* r;
+  Recipe* recipe;
+
+  virtual void SetUp() {
+    vz = new VorhandeneZutaten();
+    dv = new DeviceVerwalter(vz);
+    r = new CocktailZubereiter(dv);
+
+    recipe = new Recipe();
+    recipe->setName("Caipirinha");
+    recipe->appendStep("Limettenstuecke", 8);
+  }
+
+  virtual void TearDown() {
+    delete r;
+  }
+};
+
+TEST_F(CocktailZubereiterTest, cocktailZubereiterInitialisesCorrect){
+  EXPECT_EQ(r->myDeviceVerwalter, dv);
+}
+
+TEST_F(CocktailZubereiterTest, cocktailZubereitenDoesNotCrash){
+  EXPECT_EQ(r->cocktailZubereiten(recipe), true);
+}
