74cea14652ebd0698548ccfb432582183957fe53
Ninh, Sophia <sophia.ninh@stud.h-da.de>
2021-12-03 16:31:43 +0100

    Add CocktailProTest

---
+++ src/test/CocktailPro_Test.cpp
@@ -0,0 +1,18 @@
+#include "gtest/gtest.h"
+
+
+#include "CocktailPro.h"
+#undef protected
+#undef private
+
+class CocktailProTest : public ::testing::Test {
+ protected:
+
+  virtual void SetUp() {
+
+  }
+
+  virtual void TearDown() {
+
+  }
+};
