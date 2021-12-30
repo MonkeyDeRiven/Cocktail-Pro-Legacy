91920040ecc3befdf8da1c0d4d2bf2641df11ee0
Dirialitikus <stkaevle@stud.h-da.de>
2021-12-30 22:49:46 +0100

    USERSTORY 3

--- src/main/CocktailZubereiter.cpp
+++ src/main/CocktailZubereiter.cpp
@@ -4,31 +4,44 @@
 #include "CocktailZubereiter.h"
 
 CocktailZubereiter::CocktailZubereiter(DeviceVerwalter * dv) {
-    myDeviceVerwalter = dv;
+  myDeviceVerwalter = dv;
 }
 
 bool CocktailZubereiter::cocktailZubereiten(Recipe * rzpt, VorhandeneZutaten* ingredients) {
-    //std::system("clear");
+  //std::system("clear");
+  checkMixinPossible(rzpt, ingredients);
+  if(ausreichend) {
     std::cout << "Hallo, ich bin der CocktailZubereiter!" << std::endl
-            << "Ich habe Ihre Bestellung: " << rzpt->getName() << " erhalten." << std::endl
-            << "Jetzt geht es los!\n" << std::endl;
-	int i=0;
+              << "Ich habe Ihre Bestellung: " << rzpt->getName() << " erhalten." << std::endl
+              << "Jetzt geht es los!\n" << std::endl;
+    int i = 0;
+
     for (i = 0; i < rzpt->getNoOfRecipeSteps(); i++) {
-        RecipeStep * schritt = rzpt->getRecipeStep(i);
-        std::string zutat = schritt->getZutat();
-        float menge = schritt->getMenge();
-        float amountInGramm = amountToGramm(zutat, menge);
-        std::cout << "Rezeptschritt: " << zutat << ", " << menge << std::endl;
-        int restAmount = ingredients->getIngredientByName(zutat)->getAmount() - amountInGramm;
-        if(restAmount < 0){
-          ingredients->getIngredientByName(zutat)->setAmount(0);
-        }
-        else{
-          ingredients->getIngredientByName(zutat)->setAmount(restAmount);
-        }
-        myDeviceVerwalter->rezeptSchrittZubereiten(zutat, menge);
+
+      RecipeStep *schritt = rzpt->getRecipeStep(i);
+      std::string zutat = schritt->getZutat();
+      float menge = schritt->getMenge();
+      float amountInGramm = amountToGramm(zutat, menge);
+
+      if (ingredients->getIngredientByName(zutat)->getAmount() < amountInGramm
+          && zutat != "Stampfen" && zutat != "Mischen") {
+        std::cout << " ===== Der Cocktail kann nicht zubereitet werden, da die Zutat " << zutat
+                  << " nicht ausreichend vorhanden ist =====\n";
+        break;
+      }
+
+      std::cout << "Rezeptschritt: " << zutat << ", " << menge << std::endl;
+      int restAmount = ingredients->getIngredientByName(zutat)->getAmount() - amountInGramm;
+      if (restAmount < 0) {
+        ingredients->getIngredientByName(zutat)->setAmount(0);
+      } else {
+        ingredients->getIngredientByName(zutat)->setAmount(restAmount);
+      }
+      myDeviceVerwalter->rezeptSchrittZubereiten(zutat, menge);
     }
-  myDeviceVerwalter->myEntleerer->doIt(i);
+
+    myDeviceVerwalter->myEntleerer->doIt(i);
+  }
 
   cleanUsedDevices(rzpt);
   return (true);
@@ -36,9 +49,9 @@ bool CocktailZubereiter::cocktailZubereiten(Recipe * rzpt, VorhandeneZutaten* in
 
   //for (std::map<std::string, InternalDevice *>::iterator cleanIt = myDeviceVerwalter->myDevices->begin();
   //     cleanIt != myDeviceVerwalter->myDevices->end(); ++cleanIt) {
-   // std::cout << "Device mit der Aktion: " << cleanIt->first << " wird jetzt geputzt: " << std::endl;
+  // std::cout << "Device mit der Aktion: " << cleanIt->first << " wird jetzt geputzt: " << std::endl;
   //  cleanIt->second->putzen();
- // }
+  // }
 }
 void CocktailZubereiter::cleanUsedDevices(Recipe *rzpt) const {
   for(u_int16_t k = 0; k<rzpt->getNoOfRecipeSteps(); ++k){
@@ -65,3 +78,23 @@ CocktailZubereiter& CocktailZubereiter::operator=(CocktailZubereiter overload) {
   return *this;
 }*/
 
+void CocktailZubereiter::checkMixinPossible(Recipe *rzpt, VorhandeneZutaten *ingredients) {
+  for (int i = 0; i < rzpt->getNoOfRecipeSteps() - 1; ++i){
+
+      RecipeStep *schritt = rzpt->getRecipeStep(i);
+      std::string zutat = schritt->getZutat();
+      float menge = schritt->getMenge();
+      float amountInGramm = amountToGramm(zutat, menge);
+
+      if (zutat == "Stampfen")
+        continue;
+
+      if (ingredients->getIngredientByName(zutat)->getAmount() < amountInGramm) {
+       std::cout << " ===== Der Cocktail kann nicht zubereitet werden, da die Zutat " << zutat
+                 << " nicht ausreichend vorhanden ist =====\n";
+       ausreichend = false;
+        break;
+      }
+  }
+
+}
